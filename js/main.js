465cdd1d4ea1856f9cd24bab6c544eb952240c0d
Demchenko Vyacheslav <frontend-ninja@yandex.ru>
2019-12-01 20:25:35 +0300

    First section adaptive

--- js/main.js
+++ js/main.js
@@ -1,29 +1,58 @@
-const lamp = document.querySelector('.lamp');
-const lampLightBig = document.querySelector('.lamp-light');
-const lampLightSmall = document.querySelector('.lamp-light-small');
-const lampLightMain = document.querySelector('.lamp-light-main');
+// const lamp = document.querySelector('.lamp');
+// const lampLightBig = document.querySelector('.lamp-light');
+// const lampLightSmall = document.querySelector('.lamp-light-small');
+// const lampLightMain = document.querySelector('.lamp-light-main');
+// const headerMan = document.querySelector('.header-man__img');
 
 
-lamp.addEventListener('mouseover', () => {
-    console.log(lamp);
+// lamp.addEventListener('mouseover', lampSwitchOn);
+// lamp.addEventListener('mouseleave', lampSwitchOff);
 
-    lampLightMain.classList.remove('lamp-light-main--off');
-    lampLightBig.classList.remove('lamp-light--off');
-    lampLightSmall.classList.remove('lamp-light-small--off');
 
-    lampLightMain.classList.add('lamp-light-main--on');
-    lampLightBig.classList.add('lamp-light--on');
-    lampLightSmall.classList.add('lamp-light-small--on');
-});
+// function lampSwitchOn() {
+//     lampLightMain.classList.remove('lamp-light-main--off');
+//     lampLightBig.classList.remove('lamp-light--off');
+//     lampLightSmall.classList.remove('lamp-light-small--off');
+//     headerMan.classList.remove('header-man__img--hide');
+    
+//     headerMan.classList.add('header-man__img--show');
+//     lampLightMain.classList.add('lamp-light-main--on');
+//     lampLightBig.classList.add('lamp-light--on');
+//     lampLightSmall.classList.add('lamp-light-small--on');
+// }
 
-lamp.addEventListener('mouseleave', () => {
-    console.log(lamp);
+// function lampSwitchOff() {
+//     lampLightMain.classList.remove('lamp-light-main--on');
+//     lampLightBig.classList.remove('lamp-light--on');
+//     lampLightSmall.classList.remove('lamp-light-small--on');
+//     headerMan.classList.remove('header-man__img--show');
 
-    lampLightMain.classList.remove('lamp-light-main--on');
-    lampLightBig.classList.remove('lamp-light--on');
-    lampLightSmall.classList.remove('lamp-light-small--on');
+//     headerMan.classList.add('header-man__img--hide');
+//     lampLightMain.classList.add('lamp-light-main--off');
+//     lampLightBig.classList.add('lamp-light--off');
+//     lampLightSmall.classList.add('lamp-light-small--off');
+// }
 
-    lampLightMain.classList.add('lamp-light-main--off');
-    lampLightBig.classList.add('lamp-light--off');
-    lampLightSmall.classList.add('lamp-light-small--off');
-});
+// lamp.addEventListener('mouseover', () => {
+//     console.log(lamp);
+
+//     lampLightMain.classList.remove('lamp-light-main--off');
+//     lampLightBig.classList.remove('lamp-light--off');
+//     lampLightSmall.classList.remove('lamp-light-small--off');
+
+//     lampLightMain.classList.add('lamp-light-main--on');
+//     lampLightBig.classList.add('lamp-light--on');
+//     lampLightSmall.classList.add('lamp-light-small--on');
+// });
+
+// lamp.addEventListener('mouseleave', () => {
+//     console.log(lamp);
+
+//     lampLightMain.classList.remove('lamp-light-main--on');
+//     lampLightBig.classList.remove('lamp-light--on');
+//     lampLightSmall.classList.remove('lamp-light-small--on');
+
+//     lampLightMain.classList.add('lamp-light-main--off');
+//     lampLightBig.classList.add('lamp-light--off');
+//     lampLightSmall.classList.add('lamp-light-small--off');
+// });
